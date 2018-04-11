56eb135a19b7a84bee403b40f137eb6be0541d53
Alan Golman <golman.alan@gmail.com>
2018-04-11 17:18:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function accessElementInArray(array, index) {
   return(array[index]);
 }
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-  returns array.shift();
+  return array.shift();
 }
 
 
