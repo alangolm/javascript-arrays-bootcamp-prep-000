982c5594e2d53f4faaa87c1dcee6c081e339051d
Alan Golman <golman.alan@gmail.com>
2018-04-11 17:22:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,6 +24,9 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
+function destructivelyRemoveElementFromEndOfArray (array) {
+  return array.pop();
+}
 
 
 
